0edd741eca68ee6d2c5eec52ef19dff6609d180b
Cameron Pavey <cpave3@gmail.com>
2024-05-15 18:06:36 +1000

    chore: add test

--- sum.test.js
+++ sum.test.js
@@ -7,3 +7,8 @@ test("adds 1 + 2 to equal 3", () => {
 test("expected to fail", () => {
   expect(sum(1, 2)).toBe(4);
 });
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
